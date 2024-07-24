fe3a3f95934913ab12b64aa2a8190d045b67091f
Alberto García Arribas <100451241@alumnos.uc3m.es>
2024-07-25 00:36:35 +0200

    Initial commit: server && client side / image && font assets

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 5000;
+
+// Middleware para servir archivos estáticos
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Ruta para el frontend
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/client/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
